bcde1792fab05e0cb5eee53e752921c187a5643b
lovedeep5 <lovedeep5.abh@gmail.com>
2022-05-17 23:30:09 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const sum = "This function doing sum";
+export const sub = "This is doing sub";
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
